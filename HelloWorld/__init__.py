9a5a4c241e427e6d646eadc1ab1f3e63a4966dd4
Joe Brinkman <joseph.brinkman@earnin.com>
2019-10-02 12:40:20 -0400

    added random cat fact request

--- HelloWorld/__init__.py
+++ HelloWorld/__init__.py
@@ -1,10 +1,14 @@
 import logging
+import requests
 
 import azure.functions as func
 
-
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
+
+    logging.info("Getting random cat fact...")
+    response = requests.get("https://cat-fact.herokuapp.com/facts/random")
+    catFact = response.json()["text"]
 
     name = req.params.get('name')
     if not name:
@@ -16,7 +20,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     if name:
-        return func.HttpResponse(f"Hello {name}!")
+        return func.HttpResponse(f"Hello {name}!\n\n{catFact}")
     else:
         return func.HttpResponse(
              "Please pass a name on the query string or in the request body",
